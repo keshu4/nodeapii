4155e42d261a1ad0a8dee1925d10fd80786105ee
keshu4 <68559994+keshu4@users.noreply.github.com>
2022-04-14 23:47:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
-
+var cors = require("cors");
 var bodyParser = require("body-parser");
+
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(cors());
 app.get("/",(req,res) => {
     res.send("Hello");
 })
